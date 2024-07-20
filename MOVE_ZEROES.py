4d742856246bfa00c6e8a1c1b86096ef44abe39e
vanshikaaa <133578313+Vanshikaxxa@users.noreply.github.com>
2024-07-20 10:53:08 +0530

    Add files via upload

---
+++ MOVE_ZEROES.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        l=0
+        r=0
+        while r<len(nums):
+            if nums[r]!=0:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+            r+=1
+        return nums
